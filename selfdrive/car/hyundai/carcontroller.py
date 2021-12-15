1d123e4bc8d99d093a382c12d8e47e4040032dc5
Circuit Pro Industries <oricalworks@gmail.com>
2021-12-14 22:43:17 -0500

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -369,6 +369,15 @@ class CarController():
       stopping = (actuators.longControlState == LongCtrlState.stopping)
       can_sends.extend(create_acc_commands(self.packer, enabled, accel, jerk, int(frame / 2), lead_visible, set_speed, stopping, self.gapsetting))
 
+        # 5 Hz ACC options
+    if frame % 20 == 0 and CS.CP.radarDisablePossible:
+      can_sends.extend(create_acc_opt(self.packer))
+
+    # 2 Hz front radar options
+    if frame % 50 == 0 and CS.CP.radarDisablePossible:
+      can_sends.append(create_frt_radar_opt(self.packer))
+
+
 
     # 20 Hz LFA MFA message
     if frame % 5 == 0:
@@ -457,13 +466,4 @@ class CarController():
         can_sends.append(create_spas12(CS.mdps_bus))
       self.spas_active_last = spas_active
       self.DTQL = abs(CS.out.steeringWheelTorque)
-
-    # 5 Hz ACC options
-    if frame % 20 == 0 and CS.CP.radarDisablePossible:
-      can_sends.extend(create_acc_opt(self.packer))
-
-    # 2 Hz front radar options
-    if frame % 50 == 0 and CS.CP.radarDisablePossible:
-      can_sends.append(create_frt_radar_opt(self.packer))
-
     return can_sends
