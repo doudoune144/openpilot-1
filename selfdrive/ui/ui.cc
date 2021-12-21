fe566f819e7c2f68fe2707f3604fa9a32a8aee6b
doudoune144 <89646357+doudoune144@users.noreply.github.com>
2021-12-21 23:38:18 +0100

    update ui.cc

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -346,8 +346,8 @@ void Device::setAwake(bool on, bool reset) {
 void Device::updateBrightness(const UIState &s) {
   float clipped_brightness = BACKLIGHT_OFFROAD;
   if (s.scene.started) {
-    // Scale to 0% to 100%
-    clipped_brightness = 100.0 * s.scene.light_sensor;
+    // Scale to 0% to 100%. Set 50%
+    clipped_brightness = 50.0 * s.scene.light_sensor;
 
     // CIE 1931 - https://www.photonstophotos.net/GeneralTopics/Exposure/Psychometric_Lightness_and_Gamma.htm
     if (clipped_brightness <= 8) {
