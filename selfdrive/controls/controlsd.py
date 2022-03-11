82157251f31ffcb014148491473b7be2767a3099
doudoune144 <89646357+doudoune144@users.noreply.github.com>
2022-03-11 21:17:36 +0100

    disable unused procs
    dmDisable

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -282,12 +282,14 @@ class Controls:
     elif not self.sm.valid["pandaStates"]:
       self.events.add(EventName.usbError)
     elif not self.sm.all_alive_and_valid():
-      self.events.add(EventName.commIssue)
-      if not self.logged_comm_issue:
-        invalid = [s for s, valid in self.sm.valid.items() if not valid]
-        not_alive = [s for s, alive in self.sm.alive.items() if not alive]
-        cloudlog.event("commIssue", invalid=invalid, not_alive=not_alive)
-        self.logged_comm_issue = True
+      self.logged_comm_issue = False
+      # disable driver monitoring
+      # self.events.add(EventName.commIssue)
+      # if not self.logged_comm_issue:
+      #   invalid = [s for s, valid in self.sm.valid.items() if not valid]
+      #   not_alive = [s for s, alive in self.sm.alive.items() if not alive]
+      #   cloudlog.event("commIssue", invalid=invalid, not_alive=not_alive, can_error=self.can_rcv_error, error=True)
+      #   self.logged_comm_issue = True
     else:
       self.logged_comm_issue = False
 
